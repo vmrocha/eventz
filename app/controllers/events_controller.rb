78d817a5571c9816cd7174ac0a2541cf8eedc7af
Vinicius Rocha <vinicius.rocha@shopify.com>
2022-12-12 13:20:30 -0500

    View and Controller Part 2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,10 @@
 class EventsController < ApplicationController
   def index
-    
+    @events = [
+      "BugSmash",
+      "Hackathon",
+      "Kata Camp",
+      "Rails User Group"
+    ]
   end
 end
