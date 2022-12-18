3c1a972cf3a7bf65e19e2e18d1f409354c861fcf
Vinicius Rocha <vinicius.rocha@shopify.com>
2022-12-18 05:51:38 -0500

    Custom Queries

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ApplicationRecord
+
+  class << self
+    def upcomming
+      where("starts_at > ?", Time.now).order("starts_at")
+    end
+  end
+
   def free?
     price.blank? || price == 0
   end
