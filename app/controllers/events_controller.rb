e8b50f2e8ae7ab56f76947aaf9b94c3a8d615b8e
Vinicius Rocha <vinicius.rocha@shopify.com>
2022-12-18 04:19:20 -0500

    Forms: Editing Records Part 2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,4 +10,15 @@ class EventsController < ApplicationController
   def edit
     @event = Event.find(params[:id])
   end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :price, :starts_at)
+  end
 end
