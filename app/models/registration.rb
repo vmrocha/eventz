fe11918548acc5e3ceb7e21bb923b2773eba898e
Vinicius Rocha <vinicius.rocha@shopify.com>
2022-12-28 22:22:22 -0300

    One-to-Many: has_many

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,16 @@
 class Registration < ApplicationRecord
   belongs_to :event
+
+  HOW_HEARD_OPTIONS = [
+    "Newsletter",
+    "Blog Post",
+    "Twitter",
+    "Web Search",
+    "Friend/Coworker",
+    "Other"
+  ]
+
+  validates :name, presence: true
+  validates :email, format: { with: /\S+@\S+/ }
+  validates :how_heard, inclusion: { in: HOW_HEARD_OPTIONS }
 end
