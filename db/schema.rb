3f5cfea09237a08065c389f4301adc3f746ac011
Vinicius Rocha <vinicius.rocha@shopify.com>
2023-01-01 22:23:14 -0300

    User Account Mode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_095700) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_010935) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -31,6 +31,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_095700) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_registrations_on_event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "registrations", "events"
