8edb1a921dd2488eb1c90b03822629374a6bce8b
Vinicius Rocha <vinicius.rocha@shopify.com>
2023-01-03 14:40:42 -0300

    User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   root "events#index"
 
   resources :events do
     resources :registrations
   end
+
+  resources :users
+  get "signup" => "users#new"
 end
