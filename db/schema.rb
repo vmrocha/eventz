24a64868201fe3253f7f757016ce8baa6c68972d
Vinicius Rocha <vmrocha@gmail.com>
2023-02-07 08:38:32 -0500

    Many-to-Many with Checkboxes (1)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_012342) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_021953) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categorizations", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categorizations_on_category_id"
+    t.index ["event_id"], name: "index_categorizations_on_event_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -50,6 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_012342) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "categorizations", "categories"
+  add_foreign_key "categorizations", "events"
   add_foreign_key "likes", "events"
   add_foreign_key "likes", "users"
   add_foreign_key "registrations", "events"
