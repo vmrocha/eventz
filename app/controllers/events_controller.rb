d19ed2091b7bd767f0300bca5860caab2fbbe487
Vinicius Rocha <vmrocha@gmail.com>
2023-02-07 10:10:27 -0500

    Custom Scopes and Routes (1)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   before_action :require_admin, except: %i[index show]
 
   def index
-    @events = Event.upcomming
+    @events = Event.upcoming
   end
 
   def new
